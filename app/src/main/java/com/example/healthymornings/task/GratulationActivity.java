c87409c3543fe47df633b92d8c5505c54569d69d
capSaymon <szymonpietruszka@onet.pl>
2025-05-09 18:36:45 +0200

    efekt gukończenia zadania

---
+++ app/src/main/java/com/example/healthymornings/task/GratulationActivity.java
@@ -0,0 +1,21 @@
+package com.example.healthymornings.task;
+
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.healthymornings.R;
+
+public class GratulationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gratulation);
+
+        new Handler().postDelayed(() -> {
+            finish();
+        }, 3000);
+    }
+}
